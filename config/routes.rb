2b0cb963299b35033ee75adb77207e9e4a57f3ab
Salman Akram <thunder9ruby@gmail.com>
2015-05-26 21:06:50 -0400

    add stripe style views

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   get 'dashboard' => "pages#dashboard"
 
+  post '/buy/:slug', to: 'transactions#create', as: :buy
+
+  get '/pickup/:guid', to: 'transactions#pickup', as: :pickup
+
 end
