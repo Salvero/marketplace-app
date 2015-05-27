911e55210620701b86f3e2bb8178e47d2ddcb553
Salman Akram <thunder9ruby@gmail.com>
2015-05-26 22:18:18 -0400

    finish dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   	if current_user
   			redirect_to books_path
   	end
-    @books = Book.last(4)
+    @books = Book.last(8)
   end
 
   def about
@@ -13,5 +13,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @books = current_user.books
+    @purchased = Sale.where(buyer_email: current_user.email)
+    @sales = Sale.where(seller_email: current_user.email)
   end
 end
